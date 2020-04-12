251e589f7e4c967cc5ca0c9a4a122470523b2ef9
Anton Lokhmotov <anton@dividiti.com>
2020-04-12 14:42:40 +0100

    Specify output model name for converter.

--- package/model-openvino-convert-from-tf/install.sh
+++ package/model-openvino-convert-from-tf/install.sh
@@ -2,8 +2,9 @@
 
 ${CK_ENV_COMPILER_PYTHON_FILE} \
   ${CK_ENV_LIB_OPENVINO_MO_DIR}/mo.py \
+  --model_name ${MODEL_OUTPUT_NAME_XML} \
   --input_model ${CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH} \
   --input_shape [1,${CK_ENV_TENSORFLOW_MODEL_DEFAULT_HEIGHT},${CK_ENV_TENSORFLOW_MODEL_DEFAULT_WIDTH},3] \
-  --tensorflow_object_detection_api_pipeline_config $(dirname ${CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH})/pipeline.config \
   --reverse_input_channels \
+  --tensorflow_object_detection_api_pipeline_config $(dirname ${CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH})/pipeline.config \
   --tensorflow_use_custom_operations_config ${CK_ENV_LIB_OPENVINO_MO_DIR}/extensions/front/tf/ssd_v2_support.json
