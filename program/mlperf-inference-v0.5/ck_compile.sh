fe99b82ec4ff606e743f043f576ae6d32df75c5f
Anton Lokhmotov <anton@dividiti.com>
2020-04-12 01:12:23 +0100

    Fix ov_mlperf compilation.

--- program/mlperf-inference-v0.5/ck_compile.sh
+++ program/mlperf-inference-v0.5/ck_compile.sh
@@ -5,8 +5,8 @@ SRC_DIR=../ov_mlperf/
 
 # Configure.
 ${CK_ENV_TOOL_CMAKE_BIN}/${CK_CMAKE} ${CK_VERBOSE:-"--verbose=1"} \
--DInferenceEngine_DIR=${CK_ENV_LIB_DLDT_IE}/obj \
--DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES=${CK_ENV_LIB_DLDT_IE}/dldt/inference-engine/src/extension/ \
+-DOPENCV_DIR=${CK_ENV_LIB_OPENCV}                  \
+-DOPENVINO_DIR=${CK_ENV_LIB_OPENVINO}              \
 -DLOADGEN_LIB_DIR=${CK_ENV_LIB_MLPERF_LOADGEN_LIB} \
 -DLOADGEN_DIR=${CK_ENV_LIB_MLPERF_LOADGEN_INCLUDE} \
 -DBUILD_DIR=${BLD_DIR} \
@@ -17,8 +17,4 @@ err=$?; if [ $err != 0 ]; then exit $err; fi
 make VERBOSE=1
 err=$?; if [ $err != 0 ]; then exit $err; fi
 
-# MIsc.
-${CK_OBJDUMP} a.out > a.out.dump
-md5sum < a.out.dump > a.out.md5
-git rev-parse HEAD > a.out.git_hash 2>null
 exit 0
