2ee631b96d8ccbffbb9b99bde6a601a78f9cb0e5
Anton Lokhmotov <anton@dividiti.com>
2020-04-13 18:35:42 +0100

    Specify CMake compiler and flags.

--- program/mlperf-inference-v0.5/ck_compile.sh
+++ program/mlperf-inference-v0.5/ck_compile.sh
@@ -5,10 +5,17 @@ SRC_DIR=../ov_mlperf_cpu/
 
 # Configure.
 ${CK_ENV_TOOL_CMAKE_BIN}/${CK_CMAKE} ${CK_VERBOSE:-"--verbose=1"} \
--DOPENCV_DIR=${CK_ENV_LIB_OPENCV}                  \
--DOPENVINO_DIR=${CK_ENV_LIB_OPENVINO}              \
--DLOADGEN_LIB_DIR=${CK_ENV_LIB_MLPERF_LOADGEN_LIB} \
--DLOADGEN_DIR=${CK_ENV_LIB_MLPERF_LOADGEN_INCLUDE} \
+-DCMAKE_C_COMPILER="${CK_CC_FULL_PATH}"              \
+-DCMAKE_C_FLAGS="${CK_CC_FLAGS} ${EXTRA_FLAGS}"      \
+-DCMAKE_CXX_COMPILER="${CK_CXX_FULL_PATH}"           \
+-DCMAKE_CXX_FLAGS="${CK_CXX_FLAGS} ${EXTRA_FLAGS}"   \
+-DCMAKE_AR="${CK_AR}"                                \
+-DCMAKE_RANLIB="${CK_RANLIB}"                        \
+-DCMAKE_LINKER="${CK_LD}"                            \
+-DOPENCV_DIR="${CK_ENV_LIB_OPENCV}"                  \
+-DOPENVINO_DIR="${CK_ENV_LIB_OPENVINO}"              \
+-DLOADGEN_LIB_DIR="${CK_ENV_LIB_MLPERF_LOADGEN_LIB}" \
+-DLOADGEN_DIR="${CK_ENV_LIB_MLPERF_LOADGEN_INCLUDE}" \
 -DBUILD_DIR=${BLD_DIR} \
 ${SRC_DIR}
 err=$?; if [ $err != 0 ]; then exit $err; fi
