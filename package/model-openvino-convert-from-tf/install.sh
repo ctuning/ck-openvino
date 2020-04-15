aa74ec46809272d646bb8b13fd665c906c056e98
Anton Lokhmotov <anton@dividiti.com>
2020-04-15 13:30:03 +0100

    Print conversion command with interpolations before evaluation.

--- package/model-openvino-convert-from-tf/install.sh
+++ package/model-openvino-convert-from-tf/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+read -d '' CMD <<END_OF_CMD
 ${CK_ENV_COMPILER_PYTHON_FILE} \
   ${CK_ENV_LIB_OPENVINO_MO_DIR}/mo.py \
   --model_name ${MODEL_NAME} \
@@ -8,3 +9,9 @@ ${CK_ENV_COMPILER_PYTHON_FILE} \
   --reverse_input_channels \
   --tensorflow_object_detection_api_pipeline_config $(dirname ${CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH})/pipeline.config \
   --tensorflow_use_custom_operations_config ${CK_ENV_LIB_OPENVINO_MO_DIR}/extensions/front/tf/ssd_v2_support.json
+END_OF_CMD
+
+echo ${CMD}
+eval ${CMD}
+
+echo "Done."
