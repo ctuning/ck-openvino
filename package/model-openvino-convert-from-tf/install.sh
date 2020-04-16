4fdbd4298fa84febe7a9183325a5fbb65c9edf1c
Gavin Simpson <gavin.s.simpson@gmail.com>
2020-04-16 08:48:00 +0100

    Conditionalize optional parameters to commandline.

--- package/model-openvino-convert-from-tf/install.sh
+++ package/model-openvino-convert-from-tf/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "${CK_ENV_TENSORFLOW_MODEL_MODEL_NAME}" = "MLPerf SSD-MobileNet" ] ; then
+  CUSTOM="--tensorflow_object_detection_api_pipeline_config $(dirname ${CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH})/pipeline.config \
+      --tensorflow_use_custom_operations_config ${CK_ENV_LIB_OPENVINO_MO_DIR}/extensions/front/tf/ssd_v2_support.json"
+else
+    CUSTOM=""
+fi
+
 read -d '' CMD <<END_OF_CMD
 ${CK_ENV_COMPILER_PYTHON_FILE} \
   ${CK_ENV_LIB_OPENVINO_MO_DIR}/mo.py \
@@ -7,8 +14,7 @@ ${CK_ENV_COMPILER_PYTHON_FILE} \
   --input_model ${CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH} \
   --input_shape [1,${CK_ENV_TENSORFLOW_MODEL_DEFAULT_HEIGHT},${CK_ENV_TENSORFLOW_MODEL_DEFAULT_WIDTH},3] \
   --reverse_input_channels \
-  --tensorflow_object_detection_api_pipeline_config $(dirname ${CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH})/pipeline.config \
-  --tensorflow_use_custom_operations_config ${CK_ENV_LIB_OPENVINO_MO_DIR}/extensions/front/tf/ssd_v2_support.json
+  ${CUSTOM}
 END_OF_CMD
 
 echo ${CMD}
