cec4863f00bc46eba0d467c01dc1b3364e8bd9a8
Gavin Simpson <gavin.s.simpson@gmail.com>
2020-04-17 08:27:12 +0100

    Add support for ML_MODEL_COLOUR_CHANNELS_BGR

--- package/model-openvino-convert-from-tf/install.sh
+++ package/model-openvino-convert-from-tf/install.sh
@@ -4,7 +4,13 @@ if [ "${CK_ENV_TENSORFLOW_MODEL_MODEL_NAME}" = "MLPerf SSD-MobileNet" ] ; then
   CUSTOM="--tensorflow_object_detection_api_pipeline_config $(dirname ${CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH})/pipeline.config \
       --tensorflow_use_custom_operations_config ${CK_ENV_LIB_OPENVINO_MO_DIR}/extensions/front/tf/ssd_v2_support.json"
 else
-    CUSTOM=""
+  CUSTOM=""
+fi
+
+if [ ${ML_MODEL_COLOUR_CHANNELS_BGR:-NO} == "YES" ] ; then
+  REVERSE_INPUT_CHANNELS="--reverse_input_channels"
+else
+  REVERSE_INPUT_CHANNELS=""
 fi
 
 read -d '' CMD <<END_OF_CMD
@@ -13,7 +19,7 @@ ${CK_ENV_COMPILER_PYTHON_FILE} \
   --model_name ${MODEL_NAME} \
   --input_model ${CK_ENV_TENSORFLOW_MODEL_TF_FROZEN_FILEPATH} \
   --input_shape [1,${CK_ENV_TENSORFLOW_MODEL_IMAGE_HEIGHT},${CK_ENV_TENSORFLOW_MODEL_IMAGE_WIDTH},3] \
-  --reverse_input_channels \
+  ${REVERSE_INPUT_CHANNELS} \
   ${CUSTOM}
 END_OF_CMD
 
