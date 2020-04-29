aa50a6a8214df74e90dac889fc7df335195b2dc3
Anton Lokhmotov <anton@dividiti.com>
2020-04-29 11:40:20 +0100

    Read annotation file from Image calibration dataset.

--- package/model-openvino-convert-from-tf/install.sh
+++ package/model-openvino-convert-from-tf/install.sh
@@ -45,14 +45,13 @@ else # END OF if [ "${CK_CALIBRATE_IMAGENET}" != "yes" ]
   echo "Converting annotations ..."
   echo ""
 
-  VAL_FILE=$(basename ${CK_CAFFE_IMAGENET_VAL_TXT})
-  head -n500 ${CK_CAFFE_IMAGENET_VAL_TXT} > ${INSTALL_DIR}/${VAL_FILE}
+  ANNOTATION_FILE=${CK_DATASET_IMAGENET_CALIBRATION_ROOT}/${CK_DATASET_IMAGENET_CALIBRATION_VAL_MAP_FILE}
 
   read -d '' CMD <<END_OF_CMD
   ${CK_ENV_COMPILER_PYTHON_FILE} \
     ${CK_ENV_LIB_OPENVINO_CONVERT_ANNOTATION_PY} \
     imagenet \
-    --annotation_file ${INSTALL_DIR}/${VAL_FILE} \
+    --annotation_file ${ANNOTATION_FILE} \
     --labels_file ${CK_CAFFE_IMAGENET_SYNSET_WORDS_TXT} \
     --has_background ${CK_OPENVINO_ANNOTATION_CONVERSION_HAS_BACKGROUND} \
     --output_dir ${INSTALL_DIR}
